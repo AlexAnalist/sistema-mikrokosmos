a5da603f5ef65a6514a95a0fa1819a5a640c4c86
Alejandra Patiño <alejandra.patino@teamcore.com>
2026-04-15 23:06:53 -0400

    YOshi Vercel 0.3

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ESTO ES LO QUE FALTA:
+  base: './',
   plugins: [
     vue(),
-    vueDevTools(),
+    // Solo activamos DevTools si no estamos en producción
+    process.env.NODE_ENV !== 'production' ? vueDevTools() : []
   ],
   resolve: {
     alias: {
